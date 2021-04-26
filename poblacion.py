b198f1c8bd28af06dae1dc48846c84f5abe0e1e2
Carlos G. Gavidia <cgavidiac@gmail.com>
2021-04-26 21:23:05 +0100

    Mostrando poblacion e incremento

--- poblacion.py
+++ poblacion.py
@@ -1,6 +1,20 @@
 def iniciar():
     # Ingrese la solucion en las lineas subsiguientes.
     # INICIO
+    
+    poblacion_inicial = float(input("Ingrese poblacion actual (en miles de millones):"))
+    incremento = float(input("Ingrese tasa de crecimiento (en porcentaje):"))
+    
+    print("CRECIMIENTO POBLACIONAL ANUAL")
+    
+    
+    # 1) Poblacion: 7.98 Incremento: 0.08
+    anio = 1
+    poblacion_actual = 7.98
+    incremento_poblacion = 0.08
+    print(str(anio) + ") Poblacion: " + str(poblacion_actual) + " Incremento: " + str(incremento_poblacion) )
+    
+    
 
     # FIN
     return
