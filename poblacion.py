d5a01f671a37b1567d2aa7ab089d68d3318fc58f
Carlos G. Gavidia <cgavidiac@gmail.com>
2021-04-26 21:33:17 +0100

    Mostrar poblacion cada 100 anios

--- poblacion.py
+++ poblacion.py
@@ -2,20 +2,28 @@ def iniciar():
     # Ingrese la solucion en las lineas subsiguientes.
     # INICIO
     
-    poblacion_inicial = float(input("Ingrese poblacion actual (en miles de millones):"))
-    incremento = float(input("Ingrese tasa de crecimiento (en porcentaje):"))
+    # poblacion_inicial = float(input("Ingrese poblacion actual (en miles de millones):"))
+    # incremento = float(input("Ingrese tasa de crecimiento (en porcentaje):"))
+    
+    poblacion_inicial = 7.9
+    incremento = 1.05
     
     print("CRECIMIENTO POBLACIONAL ANUAL")
     
     
     # 1) Poblacion: 7.98 Incremento: 0.08
-    anio = 1
-    poblacion_actual = 7.98
-    incremento_poblacion = 0.08
-    print(str(anio) + ") Poblacion: " + str(poblacion_actual) + " Incremento: " + str(incremento_poblacion) )
     
-    
+    poblacion_previa = poblacion_inicial
+    for anio in range(1, 101, 1):
+        incremento_poblacion = poblacion_previa * incremento / 100
+        poblacion_actual = poblacion_previa + incremento_poblacion
 
+        print(str(anio) + ") Poblacion: " + str(round(poblacion_actual, 2)) +
+                                                " Incremento: " + str(round(incremento_poblacion, 2)))
+        poblacion_previa = poblacion_actual
+        
+    
+    
     # FIN
     return
 
